9beeedbe37110fa0fbddc0bc750805224369aee7
saikiran <juluri.saikiran@gmail.com>
2019-08-29 11:56:31 +0530

    project

---
+++ src/main/java/com/example/saikiranproject/SaikiranProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.saikiranproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaikiranProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaikiranProjectApplication.class, args);
+	}
+
+}
